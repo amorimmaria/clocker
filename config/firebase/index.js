e898ce8d397cffacd3400032cf5064adf53f6ca8
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2021-08-27 12:52:41 -0300

    Configuration firebase

---
+++ config/firebase/index.js
@@ -0,0 +1,16 @@
+
+import firebase from 'firebase/app'
+const firebaseConfig = {
+  apiKey: "AIzaSyAIDBgiVusX4yLNvqvR4yJne9UgpU2TiVc",
+  authDomain: "clocker-today.firebaseapp.com",
+  projectId: "clocker-today",
+  storageBucket: "clocker-today.appspot.com",
+  messagingSenderId: "439494245403",
+  appId: "1:439494245403:web:d33cfb0bd12d44a68e8cbe",
+  measurementId: "G-Y323ZWDBKZ"
+};
+
+// Initialize Firebase
+export default firebase = firebase.apps.lenght 
+  ? firebase.app() 
+  : firebase.initializeApp(firebaseConfig);
