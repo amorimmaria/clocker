68553a2bc996d285844814f7be07b01dd9281fcc
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2021-09-04 14:17:34 -0300

    Auth agenda Page

--- config/firebase/client.js
+++ config/firebase/client.js
@@ -19,5 +19,7 @@ const app =  firebaseClient.apps.length
 
 export const persistenceMode = firebaseClient.auth.Auth.Persistence.LOCAL
 
+export const getToken = () => firebaseClient.auth()?.currentUser.getIdToken()
+
 
 export { firebaseClient }
