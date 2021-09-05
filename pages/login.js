7b9705c573bb37dae1db9aa8d8bbcc6568e60de4
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2021-09-04 21:27:18 -0300

    Size logo

--- pages/login.js
+++ pages/login.js
@@ -56,7 +56,7 @@ export default function Login() {
 
   return (
     <Container p={4} centerContent>
-      <Logo />
+      <Logo size={150} />
       <Box p={4} mt={8}>
         <Text>Crie sua agenda compartilhada</Text>
       </Box>
